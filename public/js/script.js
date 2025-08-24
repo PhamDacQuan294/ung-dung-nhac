502ec7866a847b2c5d3d5217f3f530ac74cbda82
quan <quan93909@gmail.com>
2025-08-24 16:54:40 +0700

    Làm tính năng like

--- public/js/script.js
+++ public/js/script.js
@@ -34,3 +34,30 @@ if(aplayer) {
 
 }
 // End Aplayer
+
+// Button Like
+const buttonLike = document.querySelector("[button-like]");
+if(buttonLike) {
+  buttonLike.addEventListener("click", () => {
+    const idSong = buttonLike.getAttribute("button-like");
+    const isActive = buttonLike.classList.contains("active");
+
+    const typeLike = isActive ? "dislike" : "like";
+
+    const link = `/songs/like/${typeLike}/${idSong}`;
+
+    const option = {
+      method: "PATCH"
+    }
+
+    fetch(link, option)
+      .then(res => res.json())
+      .then(data => {
+        const span = buttonLike.querySelector("span");
+        span.innerHTML = `${data.like} thich`;
+
+        buttonLike.classList.toggle("active");
+      });
+  });
+}
+// End Button Like
