26421b5a012864677f412856d5303feb7edabebc
quan <quan93909@gmail.com>
2025-08-26 10:35:48 +0700

    Upload ảnh từ TinyMCE lên Cloudinary

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -1,28 +1,16 @@
-tinymce.init({
-  selector: 'textarea.textarea-mce',
-  plugins: 'image',
-  license_key: 'gpl',
-  file_picker_callback: (cb, value, meta) => {
-    const input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/*');
+const listTextareaMCE = document.querySelectorAll('[textarea-mce]');
 
-    input.addEventListener('change', (e) => {
-      const file = e.target.files[0];
-
-      const reader = new FileReader();
-      reader.addEventListener('load', () => {
-        const id = 'blobid' + (new Date()).getTime();
-        const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-        const base64 = reader.result.split(',')[1];
-        const blobInfo = blobCache.create(id, file, base64);
-        blobCache.add(blobInfo);
-        
-        cb(blobInfo.blobUri(), { title: file.name });
-      });
-      reader.readAsDataURL(file);
+if (listTextareaMCE.length > 0) {
+  listTextareaMCE.forEach((textarea) => {
+    const id = textarea.id;
+    tinymce.init({
+      selector: `#${id}`,
+      plugins: "image code",
+      license_key: 'gpl',
+      image_title: true,
+      images_upload_url: '/admin/upload',
+      automatic_uploads: true,
+      file_picker_types: "image",
     });
-
-    input.click();
-  },
-});
+  });
+}
