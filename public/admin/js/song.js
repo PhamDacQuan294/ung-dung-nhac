278c433c5d56cd5f8296a2507735de2d39624046
quan <quan93909@gmail.com>
2025-08-28 12:12:03 +0700

    fix form-change-multi

---
+++ public/admin/js/song.js
@@ -0,0 +1,149 @@
+// Change status
+const buttonsChangeStatus = document.querySelectorAll("[button-change-status]");
+if(buttonsChangeStatus.length > 0) {
+  const formChangeStatus = document.querySelector("#form-change-status");
+  const path = formChangeStatus.getAttribute("data-path");
+
+  buttonsChangeStatus.forEach(button => {
+    button.addEventListener("click", () => {
+      const statusCurrent = button.getAttribute("data-status");
+      const id = button.getAttribute("data-id");
+
+      let statusChange = statusCurrent == "active" ? "inactive" : "active";
+
+      const action = path + `/${statusChange}/${id}`;
+      formChangeStatus.action = action;
+
+      const redirectUrl = window.location.pathname + window.location.search;
+      formChangeStatus.action += `?_method=PATCH&redirect=${encodeURIComponent(redirectUrl)}`;
+      
+      formChangeStatus.submit();
+    });
+  });
+}
+// End Change status
+
+// Checkbox Multi
+const checkboxMulti = document.querySelector("[checkbox-multi]");
+if(checkboxMulti) {
+  const inputCheckAll = checkboxMulti.querySelector("input[name='checkall']");
+  const inputsId = checkboxMulti.querySelectorAll("input[name='id']");
+  
+  inputCheckAll.addEventListener("click", () => {
+    if(inputCheckAll.checked) {
+      inputsId.forEach(input => {
+        input.checked = true;
+      });
+    } else {
+      inputsId.forEach(input => {
+        input.checked = false;
+      });
+    }
+  });
+
+  inputsId.forEach((input) => {
+    input.addEventListener("click", () => {
+      const countChecked = checkboxMulti.querySelectorAll(
+        "input[name='id']:checked"
+      ).length;
+
+      if(countChecked == inputsId.length) {
+        inputCheckAll.checked = true;
+      } else {
+        inputCheckAll.checked = false;
+      }
+    });
+  });
+}
+// End Checkbox Multi
+
+// Form Change Multi
+const formChangeMulti = document.querySelector("[form-change-multi]");
+if(formChangeMulti) {
+  formChangeMulti.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const checkboxMulti = document.querySelector("[checkbox-multi]");
+    const inputsChecked = checkboxMulti.querySelectorAll(
+      "input[name='id']:checked"
+    );
+
+    const typeChange = e.target.elements.type.value;
+
+    if(typeChange == "delete-all") {
+      const isConfirm = confirm("Bạn có chắc muốn xoá những sản phẩm này không?");
+
+      if(!isConfirm) {
+        return;
+      }
+    }
+
+    if(inputsChecked.length > 0) {
+      let ids = [];
+      const inputIds = formChangeMulti.querySelector("input[name='ids']");
+
+      inputsChecked.forEach(input => {
+        const id = input.value;
+        if(typeChange == "change-position") {
+          const position = input.closest("tr").querySelector("input[name='position']").value;
+
+          ids.push(`${id}-${position}`);
+        } else {
+          ids.push(id);
+        }
+      });
+
+      inputIds.value = ids.join(", ");
+
+      const redirectUrl = window.location.pathname + window.location.search;
+      formChangeMulti.action += `?_method=PATCH&redirect=${encodeURIComponent(redirectUrl)}`;
+      
+      formChangeMulti.submit();
+    } else {
+      alert("Vui lòng chọn ít nhất 1 bản ghi!");
+    }
+  });
+}
+// end Form Change Multi
+
+// Delete Item
+const buttonsDelete = document.querySelectorAll("[button-delete]");
+if(buttonsDelete.length > 0) {
+  const formDeleteItem = document.querySelector("#form-delete-item");
+  const path = formDeleteItem.getAttribute("data-path");
+
+  buttonsDelete.forEach(button => {
+    button.addEventListener("click", () => {
+      const isConfirm = confirm("Bạn có chắc muốn xoá topic này không?");
+
+      if(isConfirm) {
+        const id = button.getAttribute("data-id");
+        
+        const action = `${path}/${id}`;
+
+        formDeleteItem.action = action;
+
+        const redirectUrl = window.location.pathname + window.location.search;
+        formDeleteItem.action += `?_method=DELETE&redirect=${encodeURIComponent(redirectUrl)}`;
+
+        formDeleteItem.submit();
+      }
+    });
+  }); 
+}
+// End Delete Item
+
+// Form create topic
+const formCreateTopic = document.querySelector("#form-create-topic");
+
+if(formCreateTopic) {
+  formCreateTopic.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const redirectUrl = window.location.pathname + window.location.search;
+    formCreateTopic.action += `?redirect=${encodeURIComponent(redirectUrl)}`;
+
+    formCreateTopic.submit();
+  })
+}
+// End form create topic
